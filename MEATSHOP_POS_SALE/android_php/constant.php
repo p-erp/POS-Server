b7c160b32834e0d289219293e29b783ad6444d68
Ralph Mujar <33500126+ralphmujar@users.noreply.github.com>
2019-06-19 15:24:23 +0800

    Update constant.php
    connection

--- MEATSHOP_POS_SALE/android_php/constant.php
+++ MEATSHOP_POS_SALE/android_php/constant.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-	define("DB_HOST","localhost");
+	define("DB_HOST","mysql.au.ds.network:3306");
 	define("DB_USER","ralphmujar");
 	define("DB_PASSWORD","4Gx~54el");
 	define("DB_NAME","pascalineerpcom_pos");
